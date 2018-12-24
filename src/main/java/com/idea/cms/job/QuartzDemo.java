2ec7a4270b31759bb604b6f14a6007e8ce8c0dac
Vc-lucifer <18311400747@163.com>
2018-12-24 13:58:36 +0800

    demo

--- src/main/java/com/idea/cms/job/QuartzDemo.java
+++ src/main/java/com/idea/cms/job/QuartzDemo.java
@@ -1,5 +1,6 @@
 package com.idea.cms.job;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,22 +13,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class QuartzDemo {
 
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
     @Scheduled(cron = "0 0/1 * * * ?") // 每分钟执行一次
     public void work() throws Exception {
-        System.out.println("执行调度任务： 每分钟执行一次"+new Date());
+        System.out.println("执行调度任务： 每分钟执行一次"+ format.format(new Date()));
     }
 
 
     @Scheduled(fixedRate = 5000)//每5秒执行一次
     public void play() throws Exception {
-        System.out.println("执行Quartz定时器任务："+new Date());
+        System.out.println("执行Quartz定时器任务：每五秒执行一次"+format.format(new Date()));
     }
 
 
 
     @Scheduled(cron = "0/2 * * * * ?") //每2秒执行一次
     public void doSomething() throws Exception {
-        System.out.println("每2秒执行一个的定时任务："+new Date());
+        System.err.println("每2秒执行一个的定时任务："+format.format(new Date()));
     }
 
 
@@ -35,7 +38,7 @@ public class QuartzDemo {
 
     @Scheduled(cron = "0 0 0/1 * * ? ") // 每一小时执行一次
     public void goWork() throws Exception {
-        System.out.println("每一小时执行一次的定时任务："+new Date());
+        System.out.println("每一小时执行一次的定时任务："+format.format(new Date()));
     }
 
 
